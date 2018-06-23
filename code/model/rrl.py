139fd7ece3908029a8da37c9a1711976c4e2230d
Muneeb Aadil <imuneebaadil@gmail.com>
2018-06-23 15:27:59 +0500

    generalized prev commit

--- code/model/rrl.py
+++ code/model/rrl.py
@@ -11,52 +11,24 @@ class RRL(nn.Module):
     def __init__(self, args, ckp, conv=common.default_conv): 
         super(RRL, self).__init__()
 
-        module = import_module('model.edsr')
+        module = import_module('model.' + args.model.lower())
         self.master_branch = module.make_model(args)
+
+        if not args.train_jointly: 
+            for param in branch.parameters(): 
+                param.requires_grad = False
         
-        args.n_channel_in = 64
+        args.n_channel_in = args.n_feat
         args.scale = [1]
+        if args.half_feats: 
+            args.n_feats = args.n_feats / 2 
+        if args.half_resblocks: 
+            args.n_resblocks = args.n_resblocks / 2
+        args.is_sub_mean = False
+
         self.recons_branch = module.make_model(args)
-        # args.enable_rrl = False
-
-        # for branch_num in xrange(args.n_branches): 
-        #     module = import_module('model.' + args.model.lower())
-        #     branch = module.make_model(args)
-            
-        #     if not args.train_jointly: 
-        #         for param in branch.parameters():
-        #             param.requires_grad = False
-
-        #     self.lower_branches.append(branch)
-
-        #     if args.half_feats: 
-        #         args.n_feats = args.n_feats / (2**branch_num)
-
-        #     if args.half_resblocks: 
-        #         args.n_resblocks = args.n_resblocks / (2**branch_num)
-
-        #     args.scale = [1]
-        #     args.is_sub_mean = False
-
-        # pre_train = args.pre_train 
-        # args.pre_train = '.'
-        # args.n_channel_in = 64
-        # self.higher_branch = module.make_model(args)
-
-        # args.pre_train = pre_train
 
     def forward(self, x): 
-        #IMPORTANT: ASSUMING ONLY ONE LOWER BRANCH FOR NOW..
-
-        # branch = self.lower_branches[0]
-        # self.lower_out = branch(x)
-        # print 'lower branch done'
-        # next_input = branch.featmaps
-
-        # self.higher_out = self.higher_branch(next_input)
-        # print 'higher branch done'
-        # self.final_out = self.lower_out + self.higher_out
-
         y1 = self.master_branch(x)
         y2 = self.recons_branch(self.master_branch.featmaps)
 
