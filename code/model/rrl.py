67ceb928ea76ba147d64d24a8a1a935a75c76d70
Muneeb Aadil <imuneebaadil@gmail.com>
2018-06-29 21:06:11 +0500

    generalized RRL framework to work with multiple branches

--- code/model/rrl.py
+++ code/model/rrl.py
@@ -14,33 +14,54 @@ class RRL(nn.Module):
 
         args.is_sub_mean = True 
         args_ = deepcopy(args)
-        
+
+        self.branches = []
         module = import_module('model.' + args.model.lower())
         self.master_branch = module.make_model(args)
 
         if not args.train_jointly: 
             for param in self.master_branch.parameters(): 
                 param.requires_grad = False
-        
-        args_.n_channel_in = args.n_feats
-        args_.scale = [args.scale[0] // 2] 
-        if args.half_feats: 
-            args_.n_feats = args.n_feats / 2 
-        if args.half_resblocks: 
-            args_.n_resblocks = args.n_resblocks / 2
-        args_.is_sub_mean = False
 
-        self.recons_branch = module.make_model(args_)
+        for i in xrange(args.n_branches):
+
+            args_.n_channel_in = args.n_feats
+            args_.scale = [args.scale[0] // 2] 
+            if args.half_feats: 
+                args_.n_feats = args.n_feats / 2 
+            if args.half_resblocks: 
+                args_.n_resblocks = args.n_resblocks / 2
+            args_.is_sub_mean = False
+
+            branch = module.make_model(args_)
+
+            if (not args.train_jointly) and (not (i == args.n_branches-1)): 
+                for param in branch.parameters(): 
+                    param.requires_grad = False
+
+            self.branches.append(branch)
+            self.add_module('recons_branch' if (i==0) else 'recons_branch_{}'.format(i),branch)
 
     def forward(self, x): 
+        # self.master_pred = self.master_branch(x)
+        # self.featmaps = self.master_branch.tail.modules().next()._modules['0'].outputs[0]
+        # self.refine_pred = self.recons_branch(self.featmaps)
+
         self.master_pred = self.master_branch(x)
-        self.featmaps = self.master_branch.tail.modules().next()._modules['0'].outputs[0]
-        self.refine_pred = self.recons_branch(self.featmaps)
+        
+        for i, branch in enumerate(self.branches): 
+            featmaps = self.master_branch.tail.modules().next()._modules['0'].outputs[i]
+            result = branch(featmaps)
+
+            if not (i==len(self.branches)-1): 
+                self.master_pred += result
+            else: 
+                self.refine_pred = result 
 
         return self.refine_pred
 
-    def load_state_dict(self, state_dict, strict=True): 
-        self.master_branch.load_state_dict(state_dict, strict)
+    # def load_state_dict(self, state_dict, strict=True): 
+    #     self.master_branch.load_state_dict(state_dict, strict)
 
-    def load_state_dict2(self, state_dict, strict=True):
-        self.recons_branch.load_state_dict(state_dict, strict)
+    # def load_state_dict2(self, state_dict, strict=True):
+    #     self.recons_branch.load_state_dict(state_dict, strict)
