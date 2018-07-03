3fd9712430a6cdad4803c49c41947c11a0d6961b
Muneeb Aadil <imuneebaadil@gmail.com>
2018-07-03 15:21:32 +0000

    updated demo3.sh

--- code/demo3.sh
+++ code/demo3.sh
@@ -8,27 +8,27 @@
 #python main.py --model EDSR --scale 4 --enable_rrl --half_resblocks --n_branches 1 --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x4_b1
 
 #1.1.3. half number of features
-#python main.py --model EDSR --scale 4 --enable_rrl --half_feats --n_branches 1 --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x4_b1
+python main.py --model EDSR --scale 4 --enable_rrl --half_feats --n_branches 1 --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x4_b1
 
 #1.2. scale 3 
 #1.2.1. half_both
 #python main.py --model EDSR --scale 3 --enable_rrl --half_resblocks --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_both/EDSR_baseline_x3
 
 #1.2.2. half number of layers
-#python main.py --model EDSR --scale 3 --enable_rrl --half_resblocks --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x3
+python main.py --model EDSR --scale 3 --enable_rrl --half_resblocks --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x3
 
 #1.2.3. half number of features
-#python main.py --model EDSR --scale 3 --enable_rrl --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x3
+python main.py --model EDSR --scale 3 --enable_rrl --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x3
 
 #1.3. scale 2 
 #1.3.1. half both
 #python main.py --model EDSR --scale 2 --enable_rrl --half_resblocks --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_both/EDSR_baseline_x2
 
 #1.3.2. half number of layers
-#python main.py --model EDSR --scale 2 --enable_rrl --half_resblocks --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x2
+python main.py --model EDSR --scale 2 --enable_rrl --half_resblocks --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x2
 
 #1.3.3. half number of features
-#python main.py --model EDSR --scale 2 --enable_rrl --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x2
+python main.py --model EDSR --scale 2 --enable_rrl --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x2
 
 #loss_test runs 
 #python main.py --model EDSR --scale 4 --enable_rrl --half_resblocks --half_feats --reset --pre_train ../experiment/model/EDSR_baseline_x4.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save loss_tests/charbonnier --loss 1*Charbonnier
