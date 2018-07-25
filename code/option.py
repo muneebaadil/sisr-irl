1b9f147da75f6c6f386c602ec792cf7520e02db6
Muneeb Aadil <imuneebaadil@gmail.com>
2018-07-25 06:29:08 +0000

    Revert "added upsampler for master branch in IRL, temporarily"
    This reverts commit 8d74f7ab1fde884c28e11e85d55ec92c72b9035b.

--- code/option.py
+++ code/option.py
@@ -21,7 +21,7 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed')
 
 # Data specifications
-parser.add_argument('--dir_data', type=str, default='/datadrive',
+parser.add_argument('--dir_data', type=str, default='../../../dataset',
                     help='dataset directory')
 parser.add_argument('--dir_demo', type=str, default='../test',
                     help='demo image directory')
