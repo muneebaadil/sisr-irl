b3f1ae9477d9616e73e3218f02a18302fd19a9be
Muneeb Aadil <imuneebaadil@gmail.com>
2018-07-27 11:51:24 +0500

    renamed demo_RRL to experiments.sh

--- code/experiments.sh
+++ code/experiments.sh
@@ -23,6 +23,7 @@
 
 #python main.py --dir_data /datadrive/ --ext sep --scale 4 --model SRResNet --enable_branches --n_branches 1 --branch_label residual --master_branch_pretrain ../experiment/model/SRResNet_x4.pt --loss 1*MSE --epochs 30 --save down_feats/SRResNet_x4_down --reset
 #python main.py --dir_data /datadrive/ --ext sep --scale 4 --model SRResNet --enable_branches --n_branches 1 --branch_label residual --down_feats --master_branch_pretrain ../experiment/model/SRResNet_x4.pt --loss 1*MSE --epochs 30 --save down_feats/SRResNet_x4_down --reset
+
 #1.2. scale 3 
 #python main.py --dir_data /datadrive/ --ext sep --scale 3 --model EDSR --enable_branches --half_resblocks --n_branches 1 --branch_label residual --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --loss 1*MSE --epochs 30 --save down_feats/EDSRb_x3_up --reset 
 #python main.py --dir_data /datadrive/ --ext sep --scale 3 --model EDSR --enable_branches --half_resblocks --n_branches 1 --branch_label residual --down_feats --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --loss 1*MSE --epochs 30 --save down_feats/EDSRb_x3_down --reset 
@@ -41,37 +42,3 @@
 #python main.py --dir_data /datadrive/ --ext sep --scale 4 --model EDSR --enable_branches --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --loss 1*GradL2 --epochs 30 --normalized_loss --save loss_tests/EDSRb_x4_GradL2 --reset --save_results --save_branches --save_residuals
 #python main.py --dir_data /datadrive/ --ext sep --scale 4 --model EDSR --enable_branches --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --loss 0.05*GradL2+0.95*MSE --normalized_loss --epochs 30 --save loss_tests/EDSRb_x4_05GradMSE_95MSE --reset --save_results --save_branches --save_residuals
 #python main.py --dir_data /datadrive/ --ext sep --scale 4 --model EDSR --enable_branches --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --loss 0.1*GradL2+0.9*MSE --normalized_loss --epochs 30 --save loss_tests/EDSRb_x4_10GradMSE_90MSE --reset --save_results --save_branches --save_residuals
-
-#2.1. LapSRN
-
-#1. HALVING EXPERIMENTS
-#1.1. scale 4
-#1.1.1. half_both
-#python main.py --model EDSR --scale 4 --enable_branches --half_resblocks --half_feats --n_branches 1 --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_both/EDSR_baseline_x4_b1
-#python main.py --model EDSR --scale 4 --enable_branches --half_resblocks --half_feats --n_branches 2 --reset --pretrain ../experiment/half_tests_new/half_both/EDSR_baseline_x4_b1/ --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_both/EDSR_baseline_x4_b2
-
-#1.1.2.half number of layers
-#python main.py --model EDSR --scale 4 --enable_branches --half_resblocks --n_branches 1 --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x4_b1
-
-#1.1.3. half number of features
-#python main.py --model EDSR --scale 4 --enable_branches --half_feats --n_branches 1 --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x4.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x4_b1
-
-#1.2. scale 3 
-#1.2.1. half_both
-#python main.py --model EDSR --scale 3 --enable_branches --half_resblocks --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_both/EDSR_baseline_x3
-
-#1.2.2. half number of layers
-#python main.py --model EDSR --scale 3 --enable_branches --half_resblocks --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x3
-
-#1.2.3. half number of features
-#python main.py --model EDSR --scale 3 --enable_branches --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x3.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x3
-
-#1.3. scale 2 
-#1.3.1. half both
-#python main.py --model EDSR --scale 2 --enable_branches --half_resblocks --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_both/EDSR_baseline_x2
-
-#1.3.2. half number of layers
-#python main.py --model EDSR --scale 2 --enable_branches --half_resblocks --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_layers/EDSR_baseline_x2
-
-#1.3.3. half number of features
-#python main.py --model EDSR --scale 2 --enable_branches --half_feats --reset --master_branch_pretrain ../experiment/model/EDSR_baseline_x2.pt --dir_data /datadrive/ --ext bin --print_model --n_val 10 --epochs 30 --save half_tests_new/half_feats/EDSR_baseline_x2
