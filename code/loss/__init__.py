df2f5aec1c94b69308aaa82c43f58bc70c137419
Muneeb Aadil <imuneebaadil@gmail.com>
2018-09-17 02:57:53 +0000

    Revert "added vgg feature maps scaling and fixed mean calculation"
    This reverts commit e72cd6182dec1386bcbf666944372a488545fbbe.

--- code/loss/__init__.py
+++ code/loss/__init__.py
@@ -65,8 +65,7 @@ class Loss(nn.modules.loss._Loss):
                 module = import_module('loss.vgg')
                 loss_function = getattr(module, 'VGG')(
                     loss_type[3:],
-                    rgb_range=args.rgb_range,
-                    vgg_scale=args.vgg_scale
+                    rgb_range=args.rgb_range
                 )
             elif loss_type.find('GAN') >= 0:
                 module = import_module('loss.adversarial')
