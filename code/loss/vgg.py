added vgg feature maps scaling and fixed mean calculation

code/loss/vgg.py
@@ -7,7 +7,7 @@ import torchvision.models as models
 from torch.autograd import Variable
 
 class VGG(nn.Module):
-    def __init__(self, conv_index, rgb_range=1):
+    def __init__(self, conv_index, rgb_range=1, vgg_scale=1):
         super(VGG, self).__init__()
         vgg_features = models.vgg19(pretrained=True).features
         modules = [m for m in vgg_features]
@@ -16,16 +16,17 @@ class VGG(nn.Module):
         elif conv_index == '54':
             self.vgg = nn.Sequential(*modules[:35])
 
-        vgg_mean = (0.485, 0.456, 0.406)
+        vgg_mean = (0.485 * rgb_range, 0.456 * rgb_range, 0.406 * rgb_range)
         vgg_std = (0.229 * rgb_range, 0.224 * rgb_range, 0.225 * rgb_range)
         self.sub_mean = common.MeanShift(rgb_range, vgg_mean, vgg_std)
+        self.vgg_scale = vgg_scale
         self.vgg.requires_grad = False
 
     def forward(self, sr, hr):
         def _forward(x):
             x = self.sub_mean(x)
             x = self.vgg(x)
-            return x
+            return x/self.vgg_scale
             
         vgg_sr = _forward(sr)
         with torch.no_grad():